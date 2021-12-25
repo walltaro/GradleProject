6c0727cd8c6c2590768691213d22f20aea3721fb
Walltaro <wtr1110amb6@gmail.com>
2021-12-25 18:32:18 +0900

    Add method Calc#muliply. #2

--- lib/src/main/java/Calc.java
+++ lib/src/main/java/Calc.java
@@ -6,4 +6,8 @@ public class Calc {
   public static Integer subtract(Integer num1, Integer num2) {
     return num1 - num2;
   }
+
+  public static Integer multiply(Integer num1, Integer num2) {
+    return num1 * num2;
+  }
 }
--- lib/src/main/java/Service.java
+++ lib/src/main/java/Service.java
@@ -3,5 +3,6 @@ public class Service {
     System.out.println("Execute service.execute method.");
     System.out.println(Calc.add(4,5));
     System.out.println(Calc.subtract(4, 2));
+    System.out.println(Calc.multiply(4, 2));
   }
 }
